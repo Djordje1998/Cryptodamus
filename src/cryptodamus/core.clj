efda1a80c64975b2008d2b722c2c65923bbadde7
Đorđe Novaković <57719981+Djordje1998@users.noreply.github.com>
2024-11-18 12:13:19 +0000

    Give more then 5 predicted prices

--- src/cryptodamus/core.clj
+++ src/cryptodamus/core.clj
@@ -7,7 +7,7 @@
   (println "Hello, World!"))
 
 (defn predict-price [c i]
-  [])
+  [1 2 3 4 5 6])
 
 (predict-price :BTC :d)
 
--- test/cryptodamus/core_test.clj
+++ test/cryptodamus/core_test.clj
@@ -2,5 +2,6 @@
   (:use midje.sweet) 
   (:require [cryptodamus.core :refer :all]))
 
-(facts "Return prediction of given currency and interval"
-       (predict-price :BTC :d) =not=> nil?)
+(facts "Return prediction of given currency and interval with more then 5 prices"
+       (predict-price :BTC :d) =not=> nil?
+       (> (count (predict-price :BTC :d)) 5) => true)
