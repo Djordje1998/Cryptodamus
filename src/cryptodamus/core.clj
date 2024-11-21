47a8cdade20e4117535ee3d3143f1e1bff732f02
Đorđe Novaković <57719981+Djordje1998@users.noreply.github.com>
2024-11-21 19:45:03 +0000

    Function to check the significance of 2 segments

--- src/cryptodamus/core.clj
+++ src/cryptodamus/core.clj
@@ -7,15 +7,87 @@
   (println "Hello, World!"))
 
 (def btc {:h [10 20 30 40 50 60] :d [11 22 33 44 55 66]})
-(def eth {:h [1 2 3 4 5 6] :d [1.1 2.2 3.3 4.4 5.5 6.6]})
+(def eth {:h (vec (range 20)) :d [1.1 2.2 3.3 4.4 5.5 6.6]})
+; "Historical data on currencies"
 (def historical-data {:BTC btc :ETH eth})
 
+; "Get historical data of currency with given interval"
 (defn get-historical-data [c i]
   (i (c historical-data)))
 
+; "Find pattern in past data and return expected pattern"
 (defn predict-pattern [d]
-  d)
+  (let [s (partition 5 1 d)]
+    (print s)))
 
+(def s1 (partition 5 1 (range 20)))
+(count s1)
+(first s1)
+(rest s1)
+
+; vraca procentualnu razliku izmedju 1 i 2 s1 i s2
+(defn matching-pattern-index [s1 s2]
+  (map #(Math/abs (- %1 %2)) s1 s2))
+
+(matching-pattern-index [1 2 3] [4 5 6])
+
+; proverava da li je procentualna razlika uvek vece od x (minimalni stepen slicnosti)
+
+
+(loop [s (partition 5 1 (range 20))
+       i 0]
+  (print s)
+  (print (format "%d - " i))
+  (println 
+   (matching-pattern-index (first s) (first (rest s))))
+  (if (seq s)
+    (recur (rest s) (inc i))
+    (println "End!")))
+
+; average of elements in array
+(defn avg [a]
+  (/ (apply + a) (count a)))
+
+; round number to precision
+(defn round-to [precision n]
+  (let [scale (Math/pow 10 precision)]
+    (/ (Math/round (* n scale)) scale)))
+
+(round-to 2 0.23131)
+
+; round items of array to precision
+(defn round [precision a]
+  (map (partial round-to precision) a))
+(round 3 [2.12313 5.51241 6.5111 10.5231 2.556])
+
+; [ 2 5 6 10 2]
+(def exmp [2 5 6 10 2])
+(def exmp-d [2.23 5.145 6.456 10.567 2.12412])
+(def exmp1 '(2 5 6 10 2))
+(def exmp2-d '(3.1231 8.25321 9.13412 18.3325 9.231))
+(apply + exmp1)
+(avg exmp)
+(avg exmp1)
+(avg exmp2-d)
+(Math/avg exmp)
+
+
+; array of difference in % from avg
+(defn delta-price [s]
+  (let [a (avg s)]
+     (map #(float (/ (- % a) a)) s)))
+
+(delta-price exmp-d)
+(round 3 (delta-price exmp-d))
+(round 3 (delta-price exmp2-d))
+
+(round 3 (matching-pattern-index 
+          (round 3 (delta-price exmp-d))
+          (round 3 (delta-price exmp2-d))))
+
+(predict-pattern (get-historical-data :ETH :h))
+
+; "User want to get future price of given currency base on specific interval"
 (defn predict-price [c i]
   (predict-pattern (get-historical-data c i)))
 
--- test/cryptodamus/core_test.clj
+++ test/cryptodamus/core_test.clj
@@ -1,11 +1,32 @@
 (ns cryptodamus.core-test
-  (:use midje.sweet) 
+  (:use midje.sweet)
   (:require [cryptodamus.core :refer :all]))
+
+(facts "about 'avg'"
+       (avg [1 2 3 4 5 6 7 8 9 10]) => 11/2
+       (avg [-1 -2 -3 -4 -5]) => -3
+       (avg [-1 -2 -3 4 5]) => 3/5
+       (avg '(-1 -2 -3 4 5)) => 3/5
+       (double (avg [-1 -2 -3 4 5])) => 0.6
+       (avg [1]) => 1)
+
+(facts "about 'round-to' for number"
+       (round-to 2 5.556) => 5.56
+       (round-to 1 5.556) => 5.6
+       (round-to 0 5) => 5.0
+       (round-to 2 5.554) => 5.55)
+
+(facts "about 'round' for array"
+       (round 2 [1.555 2.556 3.554]) => [1.56 2.56 3.55]
+       (round 2 [3.555555 4.555556 5.555554]) => [3.56 4.56 5.56]
+       (round 2 [1.11111 2.22222 3.33333]) => [1.11 2.22 3.33]
+       (round 1 '(1.11111 2.22222 3.33333)) => [1.1 2.2 3.3]
+       (round 0 [1.11111 2.22222 3.33333]) => [1.0 2.0 3.0])
 
 (facts "about 'get-historical-data'"
        (fact "return not null"
-        (get-historical-data :BTC :d) =not=> nil?
-        (get-historical-data :ETH :h) =not=> nil?)
+             (get-historical-data :BTC :d) =not=> nil?
+             (get-historical-data :ETH :h) =not=> nil?)
        (fact "return more then 5"
              (> (count (get-historical-data :BTC :d)) 5) => true
              (> (count (get-historical-data :ETH :h)) 5) => true))
@@ -13,14 +34,13 @@
 (facts "about 'predict-pattern'"
        (fact "return not null"
              (predict-pattern (get-historical-data :BTC :d)) =not=> nil?
-       (fact "return more then 5"
-             (> (count (predict-pattern (get-historical-data :BTC :d))) 5) => true)))
-
+             (fact "return more then 5"
+                   (> (count (predict-pattern (get-historical-data :BTC :d))) 5) => true)))
 
 (facts "about 'predict-price'"
        (fact "return not null"
-        (predict-price :BTC :d) =not=> nil?
-        (predict-price :ETH :h) =not=> nil?)
+             (predict-price :BTC :d) =not=> nil?
+             (predict-price :ETH :h) =not=> nil?)
        (fact "return more then 5"
              (> (count (predict-price :BTC :d)) 5) => true
              (> (count (predict-price :ETH :h)) 5) => true))
