48e649b065cf0b98092cc4d56a10823fc9505f42
Djordje <djordjen7@gmail.com>
2025-01-30 01:40:23 +0100

    Add project dependencies for GUI and charting libraries

--- project.clj
+++ project.clj
@@ -3,12 +3,15 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.11.1"] 
+  :dependencies [[org.clojure/clojure "1.11.1"]
                  [midje "1.10.10"]
                  [clj-http "3.13.0"]
-                 [cheshire "5.13.0"]]
+                 [cheshire "5.13.0"]
+                 [seesaw "1.5.0"]
+                 [org.jfree/jfreechart "1.5.3"]
+                 [com.toedter/jcalendar "1.4"]]
   :plugins [[lein-midje "3.2.1"]]
-  :main ^:skip-aot cryptodamus.core
+  :main cryptodamus.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
                        :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
